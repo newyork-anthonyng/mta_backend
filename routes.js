0c12aba7d82fe4a830d8c2bd016b1810760cee0b
Anthony Ng <newyork.anthonyng@gmail.com>
2016-10-31 15:48:48 -0400

    Add all_stops route

--- routes.js
+++ routes.js
@@ -3,10 +3,21 @@ const router = express.Router();
 const config = require('./config');
 const Mta = require('mta-gtfs');
 
-router.get('/delays', (req, res) => {
-	const mta = new Mta({
-		key: config.MTA_API
+router.get('/all_stops', (req, res) => {
+	const mta = createNewMta();
+
+	mta.stop().then((result) => {
+		const filteredResult = filterAllStopsData(result);
+
+		res.json({
+			SUCCESS: true,
+			result: filteredResult
+		});
 	});
+});
+
+router.get('/delays', (req, res) => {
+	const mta = createNewMta();
 
 	mta.status().then((result) => {
 		const filteredResult = filterByDelays(result);
@@ -18,13 +29,40 @@ router.get('/delays', (req, res) => {
 	});
 });
 
+function createNewMta() {
+	return new Mta({
+		key: config.MTA_API
+	});
+}
+
+function filterAllStopsData(info) {
+	const filteredInfo = Object.assign({}, info);
+
+	for(let stop in filteredInfo) {
+		if(filteredInfo.hasOwnProperty(stop)) {
+			const currentStop = filteredInfo[stop];
+
+			const updatedInfo = {
+				stop_id: currentStop.stop_id,
+				stop_name: currentStop.stop_name,
+				stop_lat: currentStop.stop_lat,
+				stop_lon: currentStop.stop_lon
+			};
+			filteredInfo[stop] = updatedInfo;
+		}
+	}
+
+	return filteredInfo;
+}
+
 function filterByDelays(info) {
 	const filteredInfo = Object.assign({}, info);
-	const transportationTypes = Object.keys(filteredInfo);
 
-	transportationTypes.forEach((type) => {
-		filteredInfo[type] = filterOutStatus(filteredInfo[type], 'GOOD SERVICE');
-	});
+	for(let type in filteredInfo) {
+		if(filteredInfo.hasOwnProperty(type)) {
+			filteredInfo[type] = filterOutStatus(filteredInfo[type], 'GOOD SERVICE');
+		}
+	}
 
 	return filteredInfo;
 }
