1e1f0be9b2c08ef4a54ed6a4c993ec53f9ac2657
Anthony Ng <newyork.anthonyng@gmail.com>
2016-10-31 16:33:07 -0400

    Start adding schedule route

--- routes.js
+++ routes.js
@@ -29,6 +29,68 @@ router.get('/delays', (req, res) => {
 	});
 });
 
+router.get('/test', (req, res) => {
+	const mta = createNewMta();
+
+	mta.schedule(635).then((result) => {
+		res.json({
+			result: result
+		});
+	});
+});
+
+router.get('/schedule', (req, res) => {
+	const mta = createNewMta();
+
+	mta.stop().then((stops) => {
+		const allPromises = [];
+		const allSchedules = [];
+		stops = {
+			635: {
+				stop_id: "635",
+				stop_name: "14 St - Union Sq",
+				stop_lat: "40.734673",
+				stop_lon: "-73.989951"
+			},
+			636: {
+				stop_id: "636",
+				stop_name: "Astor Pl",
+				stop_lat: "40.730054",
+				stop_lon: "-73.99107"
+			}
+		};
+
+		for(let stop in stops) {
+			if(stops.hasOwnProperty(stop)) {
+				const currentPromise = mta.schedule(Number(stop)).then((result) => {
+					if(result.hasOwnProperty('schedule')) {
+						for(let direction in result['schedule'][stop]) {
+							let currentTrains = result['schedule'][stop][direction];
+
+							currentTrains.forEach((train) => {
+								delete train['departureTime'];
+								train['direction'] = direction;
+								train['stop_name'] = stops[stop]['stop_name'];
+							});
+
+							allSchedules.push(currentTrains);
+						}
+					}
+				});
+
+				allPromises.push(currentPromise);
+			}
+		}
+
+		Promise.all(allPromises).then(values => {
+			res.json({
+				SUCCESS: true,
+				result: allSchedules
+			});
+		});
+	});
+});
+
 function createNewMta() {
 	return new Mta({
 		key: config.MTA_API
